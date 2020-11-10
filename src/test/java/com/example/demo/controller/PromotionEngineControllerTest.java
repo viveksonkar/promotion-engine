28eac6ab4d6cfd334726b1657cff8caa2e871027
Vivek Sonkar <viveksonkar19n@gmail.com>
2020-11-10 12:12:16 +0100

    included database configuration

--- src/test/java/com/example/demo/controller/PromotionEngineControllerTest.java
+++ src/test/java/com/example/demo/controller/PromotionEngineControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.models.CheckOutItem;
 import com.example.demo.service.CheckoutService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -20,15 +26,44 @@ public class PromotionEngineControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectmapper;
+
     @MockBean
     CheckoutService checkoutService;
 
     @Test
-    public void testCalculatePromotion() throws Exception {
-
-        mvc.perform(get("/promotion/calculate")
+    public void getPrice() throws Exception {
+        mvc.perform(post("/promotion/getPrice")
+                .content(getBasket())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
 
+
+    public String getBasket() throws JsonProcessingException {
+        List<CheckOutItem> checkOutItems = new ArrayList<>();
+
+        CheckOutItem checkOutItemA = new CheckOutItem();
+        checkOutItemA.setSku("A");
+        checkOutItemA.setQuantity(5);
+
+        CheckOutItem checkOutItemB = new CheckOutItem();
+        checkOutItemB.setSku("B");
+        checkOutItemB.setQuantity(5);
+
+        CheckOutItem checkOutItemC = new CheckOutItem();
+        checkOutItemC.setSku("C");
+        checkOutItemC.setQuantity(1);
+
+        CheckOutItem checkOutItemD = new CheckOutItem();
+        checkOutItemD.setSku("D");
+        checkOutItemD.setQuantity(1);
+        checkOutItems.add(checkOutItemA);
+        checkOutItems.add(checkOutItemB);
+        checkOutItems.add(checkOutItemC);
+        checkOutItems.add(checkOutItemD);
+
+        return objectmapper.writeValueAsString(checkOutItems);
     }
 }
